c4c44be667a55823a72f2daa3719617ff560b8b5
OC-AAN <kshillito@ocde.us>
2026-06-03 14:17:03 -0700

    Update california-scenes.js

--- assets/js/california-scenes.js
+++ assets/js/california-scenes.js
@@ -7,7 +7,7 @@ const sceneImages = [
   "assets/images/california/hollywood.png",
   "assets/images/california/mt-rubidoux.png",
   "assets/images/california/golden-gate.png",
-  "assets/images/california/sierra-lake.png",
+  "assets/images/california/sierras.png",
   "assets/images/california/central-coast.png",
   "assets/images/california/tower-bridge.png"
 ];
